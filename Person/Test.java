283ee874e59e098a4987e4932c8c7fade1a22d92
KimNgan1205 <hoalangnu2005@gmail.com>
2024-11-13 23:42:26 +0700

    Test.java

--- Person/Test.java
+++ Person/Test.java
@@ -2,15 +2,15 @@ package Person;
 
 public class Test {
 	public static void main(String[] args) {
-		Person person1 = new Person("Nguyen Van A", "01/01/1990", "Ha Noi", 'M', "vana@gmail.com", "0123456789");
+		Person person1 = new Person("Nguyen Van A", "13/11/1998", "Ha Noi", 'M', "ngvan@gmail.com", "0123456789");
 
 		System.out.println(person1.toString());
 		Person person2 = new Person();
-		person2.setName("Le Thi B");
-		person2.setDob("15/05/1992");
-		person2.setPob("Ho Chi Minh");
+		person2.setName("Nguyen Thi B");
+		person2.setDob("14/11/1998");
+		person2.setPob("Ha Noi");
 		person2.setGender('F');
-		person2.setEmail("lethi.b@gmail.com");
+		person2.setEmail("ngthi.b@gmail.com");
 		person2.setPhone("0987654321");
 
 		System.out.println(person2.toString());
